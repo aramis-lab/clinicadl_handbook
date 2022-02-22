0f64eabf6c6ade81e48d85c30e21a73ca6990407
Mauricio DIAZ <mauricio.diaz@inria.fr>
2022-02-22 16:45:33 +0100

    Add tsvtools script and notebook

--- src/tsvtools.py
+++ src/tsvtools.py
@@ -6,7 +6,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.5'
-#       jupytext_version: 1.7.1
+#       jupytext_version: 1.11.5
 #   kernelspec:
 #     display_name: Python 3
 #     name: python3
