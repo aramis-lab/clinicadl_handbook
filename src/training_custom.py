b220d9d9e4c114fb82d73647214ad1b806dc0190
Mauricio Diaz <mauricio.diaz@inria.fr>
2023-03-14 15:05:27 +0100

    Edit text style and typos in training_custom

--- src/training_custom.py
+++ src/training_custom.py
@@ -14,9 +14,7 @@
 
 # %% [markdown]
 # # Training for custom task
-
-# %% [markdown]
-# (custom_exp)=
+#
 # ## Customize your experiment!
 
 # You want to train your custom architecture, with a custom input type or
@@ -24,7 +22,8 @@
 # repo](https://github.com/aramis-lab/clinicadl]) to add your changes.
 
 
-# ### Add a custom model <i class="fa fa-hourglass-start " aria-hidden="true"></i>
+# ### Add a custom model  <i class="fa fa-hourglass-start " aria-hidden="true"></i>
+#
 # Write your model class in `clinicadl/tools/deep_learning/models` and import it
 # in `clinicadl/tools/deep_learning/models/__init__.py`.
 
@@ -40,8 +39,10 @@
 
 # Input types that are already provided in `clinicadl` are image, patch, roi and
 # slice. To add a custom input type, please follow the steps detailed below:
-# * Choose a mode name for this input type (for example default ones are image, patch, roi and slice). 
-# * Add your dataset class in `clinicadl/tools/deep_learning/data.py` as a child class of the abstract class `MRIDataset`.
+# * Choose a mode name for this input type (for example default ones are image,
+# patch, roi and slice).
+# * Add your dataset class in `clinicadl/tools/deep_learning/data.py` as a child
+# class of the abstract class `MRIDataset`.
 # * Create your dataset in `return_dataset` by adding:
 
 # ```python
@@ -58,6 +59,7 @@
 # * Add your custom subparser to `train` and complete `train_func` in `clinicadl/cli.py`.
 
 # ### Add a custom preprocessing <i class="fa fa-hourglass-start " aria-hidden="true"></i> <i class="fa fa-hourglass-start " aria-hidden="true"></i>
+#
 # Define the path of your new preprocessing in the `_get_path` method of
 # `MRIDataset` in `clinicadl/tools/deep_learning/data.py`. You will also have to
 # add the name of your preprocessing pipeline in the general command line by
@@ -65,12 +67,13 @@
 # `train_pos_group` in `cli.py`.
 
 # ### Change the labels <i class="fa fa-hourglass-start " aria-hidden="true"></i>
-# You can launch a classification task with clinicadl using any labels. The input
-# tsv files must include the columns `participant_id`, `session_id` and
-# `diagnosis`. If the column `diagnosis` does not contain the labels described in
-# this tutorial (AD, CN, MCI, sMCI, pMCI), you can add your own label name
-# associated to a class value in the `diagnosis_code` of the class `MRIDataset` in
-# `clinicadl/tools/deep_learning/data.py`.
+#
+# You can launch a classification task with clinicadl using any labels. The
+# input tsv files must include the columns `participant_id`, `session_id` and
+# `diagnosis`. If the column `diagnosis` does not contain the labels described
+# in this tutorial (AD, CN, MCI, sMCI, pMCI), you can add your own label name
+# associated to a class value in the `diagnosis_code` of the class `MRIDataset`
+# in `clinicadl/tools/deep_learning/data.py`.
 # %% [markdown]
 # ```{admonition} Suggestion!
 # :class: tip
