7bb1f3016372e94161cd1afa029fe364ed57420f
camillebrianceau <57992134+camillebrianceau@users.noreply.github.com>
2023-04-19 10:15:23 +0200

    review

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -404,10 +404,11 @@ plotting.show()
 # `t1-volume`) containing QC results.
 
 ##
-# !!! Note:
+# :::{note}
 # Quality checks pipelines are all different and depend on the chosen
 # preprocessing. They should not be applied to other preprocessing procedures as
 # the results may not be reliable.
+# :::
 
 # %% [markdown]
 # ### Run the pipeline
